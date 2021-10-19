704677af8e90184545ce14c3592259b3f9538ecd
ng0cminh <ng0cminh@yahoo.com>
2021-10-19 13:06:41 +0700

    fix

---
+++ README.md
@@ -0,0 +1,12 @@
+---
+Languae: HTML & CSS & Javascript
+Description: Project về xây dựng giao diện HTML5, CSS3 và Javascript
+Author: ng0cminh
+---
+# Ví dụ về ngôn ngữ Markdown
+# Đây là thẻ h2
+### Đây là thẻ h3
+#### Đây là thẻ h4
+##### Đây là thẻ h5
+###### Đây là thẻ h6
+
---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const mainMenu = $(".main-menu");
+const navMobileBtn = $(".main-menu .nav-mobile-btn");
+const navOverlayElement = $(".main-menu .nav-overlay");
+const navMobileClose = $(".main-menu .nav-mobile-close");
+const mainNavbarsElement = $(".main-menu .navbars");
+
+function addShowClassNav(elementClick) {
+  elementClick.onclick = (e) => {
+    navOverlayElement.classList.toggle("show");
+    mainNavbarsElement.classList.toggle("show");
+  };
+}
+
+addShowClassNav(navMobileBtn);
+addShowClassNav(navMobileClose);
+addShowClassNav(navOverlayElement);
+
+// Menu Main Search
+const mainMenuSeach = $(".main-menu .nav-search-btn");
+const mainSeachInput = $(".main-menu .search-input");
+const mainSeachClose = $(".main-menu .nav-search-close");
+
+mainMenuSeach.onclick = (e) => {
+  mainSeachInput.classList.add("show");
+  mainSeachClose.classList.add("show");
+  mainMenuSeach.classList.remove("show");
+  mainSeachInput.focus();
+};
+
+mainSeachClose.onclick = (e) => {
+  mainSeachInput.classList.remove("show");
+  mainSeachClose.classList.remove("show");
+  mainMenuSeach.classList.add("show");
+};
+
+const sidebar = $(".sidebar");
+const tableContent = $(".table-content");
+
+tableContent.onclick = (e) => {
+  sidebar.classList.toggle("show");
+  tableContent.classList.toggle("show");
+};
